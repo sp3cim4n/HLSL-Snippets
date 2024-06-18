2ca030a1b0b618a7ff595250d0b88c15e1473682
Ashish Nayak <44918844+sp3cim4n@users.noreply.github.com>
2024-06-18 12:28:42 +0530

    TriplanerUV

---
+++ TriplanerUV.hlsl
@@ -0,0 +1,11 @@
+/** Triplaner UV */
+float2 TriplanerUV(float3 N, float3 Pos)
+{
+  N = abs(N);
+
+  float3 MaskXY = lerp(float3(1,0,0), float(0,1,0), step(N.x,N.y));
+  float3 MaskXYZ = lerp(XY, float(0,0,1), step(max(N.x,N.y), N.z));
+
+  //UV = xplane*xmask + yplane*ymask + zplane*zmask
+  return Pos.yz*MaskXYZ.x + Pox.xz*MaskXYZ.y + Pos.xy*MaskXYZ.z
+}
